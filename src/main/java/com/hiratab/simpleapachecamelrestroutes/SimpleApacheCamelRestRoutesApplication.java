53f14eb9264dd91491f4f41d17e79b910457b5ca
Bruno Hirata <brn.hirata@gmail.com>
2023-06-19 11:16:38 +0100

    Initial commit

---
+++ src/main/java/com/hiratab/simpleapachecamelrestroutes/SimpleApacheCamelRestRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.hiratab.simpleapachecamelrestroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApacheCamelRestRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApacheCamelRestRoutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiratab/simpleapachecamelrestroutes/SimpleApacheCamelRestRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiratab.simpleapachecamelrestroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApacheCamelRestRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
